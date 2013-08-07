e3040a7c191ccc9f97d26da9135fcd7b494cf312
Uchiko <memememomo@gmail.com>
2013-08-08 01:36:06 +0900

    first import

---
+++ lib/mojo/event_emitter.rb
@@ -0,0 +1,97 @@
+module Mojo
+  class EventEmitter
+    DEBUG = ENV["MOJO_EVENTEMITTER_DEBUG"] || 0
+
+    def initialize
+      @events = Hash.new
+    end
+
+    def emit( *args )
+      name = args.shift
+      if s = @events[name] then
+        warn "-- Emit #{name} in \n" if DEBUG == 1
+        s.each do |cb|
+          cb.call(args.clone.unshift(self))
+        end
+      else
+        warn "-- Emit #{name} in " if DEBUG == 1
+        warn args[0] if name == "error"
+      end
+
+      self
+    end
+
+    def emit_safe( *args )
+      name = args.shift
+
+      if s = @events[name] then
+        warn "-- Emit #{name} in \n" if DEBUG == 1
+        s.each do |cb|
+          begin
+            cb.call(args.clone.unshift(self))
+          rescue Exception => ex
+            if name == "error" then
+              # Error event failed
+              warn "Event \"error\" failed: " + ex.message
+            else
+              # Normal event failed
+              self.emit_safe('error', "Event \"#{name}\" failed: " + ex.message)
+            end
+          end
+        end
+      else
+        warn "-- Emit #{name} in \n" if DEBUG == 1
+        warn args[0] if name == "error"
+      end
+
+      self
+    end
+
+    def has_subscribers(name)
+      !!self.subscribers(name).length
+    end
+
+    def on(name, cb)
+      @events[name] ||= Array.new
+      @events[name].push(cb)
+      cb
+    end
+
+    def once(*args)
+      name = args.shift
+      cb = args.shift
+
+      wrapper = proc {
+        self.unsubscribe(name, wrapper)
+        cb.call(args.clone.unshift(self))
+      }
+      self.on(name, wrapper)
+
+      wrapper
+    end
+
+    def subscribers(name)
+      @events[name] ||= Array.new
+      @events[name]
+    end
+
+    def unsubscribe(name, cb)
+
+      if cb then
+        # One
+        l = Array.new
+        @events[name].each{|o|
+          if o != cb then
+            l.push(o)
+          end
+        }
+        @events[name] = l
+      else
+        # All
+        @events.delete(name)
+      end
+
+      self
+    end
+  end
+end
---
+++ spec/mojo/eventemitter.rb
@@ -0,0 +1,129 @@
+require 'rubygems'
+require 'rspec'
+require 'tempfile'
+
+require 'mojo/event_emitter.rb'
+
+describe "Mojo::EventEmitter" do
+
+  before do
+    @e = Mojo::EventEmitter.new
+    @called = 0
+      @echo = ""
+      @err = ""
+  end
+
+  context 'Normal event' do
+    it do
+      @e.on('test1', proc { @called += 1 })
+      @e.emit('test1')
+      expect(@called).to eq(1)
+
+
+
+      @e.on("die", proc { raise "works!\n" })
+      expect { @e.emit('die') }.to raise_error("works!\n")
+
+
+
+      @tempfile = Tempfile.open("stdouttest")
+      $stderr = File.open(@tempfile, "w")
+      @e.emit("error", "just")
+      @e.emit_safe("error", "works")
+      $stderr.flush
+      $stderr = STDERR
+      @error = "";
+      File.open(@tempfile) {|f|
+        @error += f.read
+      }
+      expect(@error).to eq("just\nworks\n")
+
+
+
+
+      @e.on('error', proc {|args| @err = args.pop })
+      @e.on('test2', proc {|args| @echo += 'echo: ' + args.pop })
+      @e.on('test2', proc {|args|
+              msg = args.pop
+              raise "test2: #{msg}\n"
+            })
+      cb = proc {|args| @echo += 'echo2: ' + args.pop }
+      @e.on('test2', cb)
+      @e.emit_safe('test2', 'works!')
+      expect(@echo).to eq("echo: works!echo2: works!")
+      @echo = ""
+      @err = ""
+      expect(@e.subscribers('test2').length).to eq(3)
+      @e.unsubscribe('test2', cb)
+      expect(@e.subscribers('test2').length).to eq(2)
+      @e.emit_safe('test2', 'works!')
+      expect(@echo).to eq("echo: works!")
+      expect(@err).to eq("Event \"test2\" failed: test2: works!\n")
+
+
+
+
+      # Normal event again
+      @e.emit('test1')
+      expect(@called).to eq(2)
+      expect(@e.subscribers('test1').length).to eq(1)
+      @e.emit('test1')
+      @e.unsubscribe('test1', @e.subscribers('test1')[0])
+      expect(@called).to eq(3)
+      expect(@e.subscribers('test1').length).to eq(0)
+      @e.emit('test1')
+      expect(@called).to eq(3)
+      @e.emit('test1')
+      expect(@called).to eq(3)
+
+
+
+      # One-time event
+      @once = 0
+      @e.once('one_time', proc { @once += 1 })
+      expect(@e.subscribers('one_time').length).to eq(1)
+      @e.emit('one_time')
+      expect(@once).to eq(1)
+      expect(@e.subscribers('one_time').length).to eq(0)
+      @e.emit('one_time')
+      expect(@once).to eq(1)
+      @e.emit('one_time')
+      expect(@once).to eq(1)
+      @e.emit('one_time')
+      expect(@once).to eq(1)
+      @e.once('one_time', proc {|args|
+                obj = args.shift
+                obj.once('one_time', proc { $once += 1 })
+              })
+      @e.emit('one_time');
+      expect(@once).to eq(1)
+    end
+  end
+
+  context 'Error' do
+    it do
+    end
+  end
+
+  context "Unhandled error event" do
+    before do
+    end
+
+    it do
+    end
+  end
+
+  context "Error fallback" do
+    before do
+    end
+
+    it do
+    end
+  end
+
+  context "Normal event again" do
+    it do
+    end
+  end
+
+end
